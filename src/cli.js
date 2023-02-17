3f8b604b6c523c0bf193b9ec550aae9bdf89069b
Marina Yakubovskaya <maribars93@yandex.ru>
2023-02-17 15:37:30 +0300

    Install & setup eslint, add lint to Makefile

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,8 @@
 import readlineSync from 'readline-sync';
 
-
 export default function sayHello() {
-	const userName = readlineSync.question('May I have your name? ');
-	const greeting = `Hello, ${userName}!`;
+  const userName = readlineSync.question('May I have your name? ');
+  const greeting = `Hello, ${userName}!`;
 
-	console.log(greeting);
-};
-
+  console.log(greeting);
+}
