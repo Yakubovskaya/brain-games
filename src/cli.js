2d2c030bcdbf9646f1a67ba6961807d10c7ff286
Marina Yakubovskaya <maribars93@yandex.ru>
2023-02-17 14:45:02 +0300

    Add greeting function to cli.js

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+
+export default function sayHello() {
+	const userName = readlineSync.question('May I have your name? ');
+	const greeting = `Hello, ${userName}!`;
+
+	console.log(greeting);
+};
+
