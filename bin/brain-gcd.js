67b7c6e3c5f44ce9dd5d925094f6c3be9003c538
Marina Yakubovskaya <maribars93@yandex.ru>
2023-03-14 12:15:59 +0300

    Create Brain Gcd game

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,1 +1,4 @@
 #!/usr/bin/env node
+import playGcdGame from '../src/games/gcd_game.js';
+
+playGcdGame();
---
+++ src/games/gcd_game.js
@@ -0,0 +1,23 @@
+import getRandomIntInclusive from '../get_random_int.js';
+import playGames from '../index.js';
+
+function generateRoundofGcdGame() {
+  let firstNumber = getRandomIntInclusive(1, 50);
+  let secondNumber = getRandomIntInclusive(1, 50);
+  const question = `${firstNumber} ${secondNumber}`;
+  while (firstNumber !== secondNumber) {
+    if (firstNumber > secondNumber) {
+      firstNumber -= secondNumber;
+    } else {
+      secondNumber -= firstNumber;
+    }
+  }
+  const rightAnswer = firstNumber;
+
+  return [question, rightAnswer];
+}
+
+export default function playGcdGame() {
+  const rulesOfEvenGame = 'Find the greatest common divisor of given numbers.';
+  playGames(rulesOfEvenGame, generateRoundofGcdGame);
+}
