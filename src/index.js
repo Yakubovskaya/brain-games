7536c6a1d6622484c021b848b7586c89209e0aa2
Marina Yakubovskaya <maribars93@yandex.ru>
2023-05-01 16:26:02 +0300

    Add function getGreetingandUserName

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
+import getGreetingandUserName from './greeting.js';
 
 const playGames = (description, generateGame) => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  const userName = getGreetingandUserName();
   console.log(description);
   for (let i = 0; i < 3; i += 1) {
     const [question, rightAnswer] = generateGame();
